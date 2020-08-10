2165b90b92f66344f02c4f6b54cc5d9b0a1bc075
wannasiri <wannasirikanpaiboon12@gmail.com>
2020-08-10 17:54:10 +0700

    First commit

---
+++ src/week6/Token101.java
@@ -0,0 +1,11 @@
+package week6;
+
+public class Token101 {
+	public static void main(String[]args) {
+		int x,y,sum;
+		x = 5;
+		y = 4;
+		sum = x+y;
+		System.out.println("Total x + y = " + sum);
+	}
+}
